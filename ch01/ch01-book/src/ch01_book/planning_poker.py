43d6b6c5826666e22075b2064fcc71d237c02e5a
herbert <h.beemster@nki.nl>
2023-09-23 21:11:06 +0200

    - implement ch01-book

--- ch01/ch01-book/src/ch01_book/planning_poker.py
+++ ch01/ch01-book/src/ch01_book/planning_poker.py
@@ -5,11 +5,30 @@ from ch01_book.esitmate import Estimate
 
 
 class PlanningPoker:
+    """PlanningPoker class."""
 
+    # ------------------------------------------------------------------------
     @staticmethod
-    def identify_extremes(estimates: List[Estimate] )->Tuple[str, str]:
+    def identify_extremes(estimates: List[Estimate]) -> List[str]:
         """Returns the extremes of the given estimates."""
 
-        if not estimates:
-            raise ValueError("Estimates can't be None.");
+        if estimates is None:
+            raise ValueError("Estimates can't be None")
 
+        if len(estimates) <= 1:
+            raise ValueError("There has to be more than 1 estimate in the list")
+
+        lowest_estimate = None
+        highest_estimate = None
+
+        for estimate in estimates:
+            if highest_estimate is None or estimate.estimate > highest_estimate.estimate:
+                highest_estimate = estimate
+
+            if lowest_estimate is None or estimate.estimate < lowest_estimate.estimate:
+                lowest_estimate = estimate
+
+        if lowest_estimate == highest_estimate:
+            return []
+
+        return [lowest_estimate.developer, highest_estimate.developer]
--- ch01/ch01-book/tests/test_planning_poker.py
+++ ch01/ch01-book/tests/test_planning_poker.py
@@ -1,9 +1,65 @@
 """Module to test the PlanningPoker class."""
+from ch01_book.esitmate import Estimate
 from ch01_book.planning_poker import PlanningPoker
 import pytest
 
 
-def test_reject_none_input():
-    with pytest.raises(ValueError, match="Estimates can't be None.") as e:
-        PlanningPoker.identify_extremes(None)
+# ------------------------------------------------------------------------
+@pytest.mark.parametrize(
+    "value, expected_message",
+    [
+        (None, "Estimates can't be None"),
+        ([], "There has to be more than 1 estimate in the list"),
+        ([Estimate("Eleanor", 1)], "There has to be more than 1 estimate in the list"),
+    ],
+    ids=[
+        "reject_none_input",
+        "reject_empty_list",
+        "reject_single_estimate",
+    ],
+)
+# ------------------------------------------------------------------------
+def test_inputs(value, expected_message):
+    with pytest.raises(ValueError, match=expected_message) as e:
+        PlanningPoker.identify_extremes(value)
 
+
+# ------------------------------------------------------------------------
+def test_two_estimates():
+
+    devs = PlanningPoker.identify_extremes(
+        [
+            Estimate("Mauricio", 10),
+            Estimate("Frank", 5),
+        ]
+    )
+
+    assert {"Mauricio", "Frank"} == set(devs)
+
+
+# ------------------------------------------------------------------------
+def test_many_estimates():
+
+    devs = PlanningPoker.identify_extremes(
+        [
+            Estimate("Mauricio", 10),
+            Estimate("Arie", 5),
+            Estimate("Frank", 7),
+        ]
+    )
+
+    assert {"Mauricio", "Arie"} == set(devs)
+
+# ------------------------------------------------------------------------
+def test_all_developers_with_the_same_estimate():
+    devs = PlanningPoker.identify_extremes(
+        [
+            Estimate("Mauricio", 10),
+            Estimate("Arie", 10),
+            Estimate("Andy", 10),
+            Estimate("Frank", 10),
+            Estimate("Annibale", 10),
+        ]
+    )
+
+    assert [] == devs
